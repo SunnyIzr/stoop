0a61230c40d6aa639cc2d69c770aeb3f96cf75ee
Sunny Israni <sunny@zoraab.com>
2015-07-20 18:32:13 -0400

    adding methods to event for easy access of guestlist

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ActiveRecord::Base
   has_many :invites
   has_many :attendees, through: :invites
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id' 
+  
+  def confirmed_guests
+  end
+  
+  def declined_guests
+  end
+  
+  def unconfirmed_guests
+  end
 end
