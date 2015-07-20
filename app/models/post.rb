9cbdb6c659f07ffebcc82ffa91d302da3ae892b7
Sunny Israni <sunny@zoraab.com>
2015-07-20 13:25:44 -0400

    posts now are able to attach image

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,16 @@ class Post < ActiveRecord::Base
   acts_as_mentioner
   
   belongs_to :user
+  
+  has_attached_file :image,
+                  :storage => :s3,
+                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/                  
+                  
+                  
+  def s3_credentials
+    {:bucket => ENV['BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']}
+  end
+   
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 require 'simplecov'
 SimpleCov.start
 
+require "paperclip/matchers"
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -37,6 +39,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  
+  config.include Paperclip::Shoulda::Matchers
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
