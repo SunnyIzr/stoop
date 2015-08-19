f6d18c24f103f7dab439f5f77555ed6a63e72128
Sunny Israni <sunny@zoraab.com>
2015-08-19 15:51:19 -0400

    adding comments_controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+  
+  def create
+    @comment = Comment.create(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      respond_to do |format|
+        format.json { render json: @comment}
+        format.html { redirect_to root_path }
+      end
+    else
+      respond_to do |format|
+        format.json { render nothing: true }
+        format.html { redirect_to root_path }
+      end
+    end
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:title,:comment,:commentable_id,)
+  end
+end
