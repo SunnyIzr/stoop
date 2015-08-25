83baab8dab61defc5b0bb50f42d7aeb2caf104b2
Sunny Israni <sunny@zoraab.com>
2015-08-25 15:19:26 -0400

    indicating different layouts for public facing vs internal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :choose_layout
+  
+  def choose_layout
+    current_user.nil? ? 'public_facing' : 'application'
+  end
+  
 end
