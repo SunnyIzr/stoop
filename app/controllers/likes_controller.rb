95b25b40acd493c63eabc2b7812d01c9d08d8148
Sunny Israni <sunny@zoraab.com>
2015-08-26 11:07:12 -0400

    user can now like a post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,27 @@ class LikesController < ApplicationController
   before_filter :authenticate_user!
   
   def create
-    @user = current_user
-    @user.like!(Post.find(params[:likeable_id]))
+    user = current_user
+    @post = Post.find(like_params[:likeable_id])
+    if user.likes?(@post)
+      user.unlike!(@post)
+      respond_to do |format|
+        format.json { render json: @post}
+        format.html { redirect_to root_path }
+      end      
+    else
+      user.like!(@post)
+      respond_to do |format|
+        format.json { render json: @post}
+        format.html { redirect_to root_path }
+      end
+    end
+  end
+  
+  private
+  
+  def like_params
+    params.require(:like).permit(:likeable_id)
   end
   
 end
