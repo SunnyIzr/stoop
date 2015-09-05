0e3dfbd9a77b0c0e79edb479499544184c8fc331
Sunny Israni <sunny@zoraab.com>
2015-09-05 10:37:16 -0400

    adding basic skeletong for comment js object

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,24 @@
+var CommentEvents = {
+  init: function(){
+    this.newLike();
+    this.newComment();
+    this.addCommentField();
+  },
+  addCommentField: function(){
+  },
+  newLike: function(){
+  },
+  newComment: function(){
+  }
+}
+
+var Comment = {
+  getRefresh: function(id){
+    //Gets Comment data AND refreshes view
+  },
+  refresh: function(comment){
+    // ONLY refreshes view
+  },
+  addNew: function(comment){
+  }
+}
