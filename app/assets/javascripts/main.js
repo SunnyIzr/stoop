4ea7382e0c10f8b1609aab7f13451e1b029d6143
Sunny Israni <sunny@zoraab.com>
2015-09-05 17:47:41 -0400

    adding basic chatlist view with dummy data

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
   PostEvents.init();
   CommentEvents.init();
   FollowEvents.init();
+  MessageEvents.init();
 })
---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,21 @@
+var MessageEvents = {
+  init: function(){
+    this.chatListClick()
+  },
+  chatListClick: function(){
+    $('.chat-bar .top-bar').click(function(e){
+      e.preventDefault();
+      MessageView.toggleChatList();
+    })
+  }
+}
+
+var Message = {
+}
+
+var MessageView = {
+  toggleChatList: function(){
+    $('.chat-box').toggleClass('expanded')
+  }
+  
+}
