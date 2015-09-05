58824fdb0259c1d79c7b8902ece8ed56d838e093
Sunny Israni <sunny@zoraab.com>
2015-09-05 10:37:40 -0400

    adding new attribute to private params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!
   
   def create
+    p '*'*100
+    p 'RUNNING CREATE COMMENTS'
     @comment = Comment.create(comment_params)
     @comment.user = current_user
     if @comment.save
@@ -19,6 +21,6 @@ class CommentsController < ApplicationController
   
   private
   def comment_params
-    params.require(:comment).permit(:title,:comment,:commentable_id)
+    params.require(:comment).permit(:title,:comment,:commentable_id,:commentable_type)
   end
 end
