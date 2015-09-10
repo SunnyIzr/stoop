ec68932c20ebdd4e6ef2c813951ee5bd3e8fc04a
Sunny Israni <sunny@zoraab.com>
2015-09-10 16:29:33 -0400

    coupling toget a user account that has a business. In other words, a user account that is associated with a business will not have any actual user functionality

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -7,6 +7,10 @@ class Incentive < ActiveRecord::Base
                 :storage => :s3,
                 :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
+  def self.types
+    [:cash, :percent]
+  end
+  
   def s3_credentials
     {:bucket => ENV['BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']}
   end
