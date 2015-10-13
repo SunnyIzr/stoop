616d13e7fc39af49f6a8dc543dc5e5b49c06ce55
Sunny Israni <sunny@zoraab.com>
2015-10-13 19:45:17 -0400

    creating basic yelp api wrapper methods

---
+++ app/helpers/yelp_api.rb
@@ -0,0 +1,14 @@
+module YelpApi
+  extend self
+  def search(term,location)
+    Yelp.client.search(location,{term: term})
+  end
+  
+  def search_by_coords(term,coords)
+    Yelp.search_by_coordinates( { latitude: coords[0], longitude: coords[1] }, { term: term, limit: 10 } )
+  end
+  
+  def business(id)
+    Yelp.business(id)
+  end
+end
