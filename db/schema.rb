a6bd3d0e01496aa85a90b3da8ca42000691ae516
Sunny Israni <sunny@zoraab.com>
2015-10-16 17:31:28 -0400

    adding verified attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016204934) do
+ActiveRecord::Schema.define(version: 20151016213040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -254,6 +254,7 @@ ActiveRecord::Schema.define(version: 20151016204934) do
     t.datetime "cover_updated_at"
     t.text     "interests"
     t.boolean  "admin",                  default: false
+    t.boolean  "verified",               default: false
   end
 
   add_index "users", ["building_id"], name: "index_users_on_building_id", using: :btree
