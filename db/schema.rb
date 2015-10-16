312abb9af55c418272a24f9501495c1b1cd55f96
Sunny Israni <sunny@zoraab.com>
2015-10-16 16:50:03 -0400

    creating admin user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016172144) do
+ActiveRecord::Schema.define(version: 20151016204934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -223,18 +223,18 @@ ActiveRecord::Schema.define(version: 20151016172144) do
   add_index "posts", ["neighborhood_id"], name: "index_posts_on_neighborhood_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.integer  "building_id"
     t.string   "first_name"
     t.string   "last_name"
@@ -253,6 +253,7 @@ ActiveRecord::Schema.define(version: 20151016172144) do
     t.integer  "cover_file_size"
     t.datetime "cover_updated_at"
     t.text     "interests"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["building_id"], name: "index_users_on_building_id", using: :btree
