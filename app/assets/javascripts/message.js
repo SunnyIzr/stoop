dfa5bdc932b40c5da40d9844dea4738ed870644a
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-11-22 19:20:48 -0800

    send new message UI functional

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,22 @@
 var MessageEvents = {
   init: function(){
-    this.chatListClick()
+    this.chatListClick();
+    this.conversationClick();
   },
   chatListClick: function(){
     $('.chat-bar .top-bar').click(function(e){
       e.preventDefault();
       MessageView.toggleChatList();
+    })
+  },
+  conversationClick: function(){
+    $('.conversation').click(function(e){
+      var elementTargeted = $(e.currentTarget)
+      var name = elementTargeted.find(".name")[0].innerHTML
+      var imageSrc = elementTargeted.find("img")[0].getAttribute("src");
+      MessageView.hideConversations()
+      MessageView.showConversation()
+      MessageView.setConversationValues(name, imageSrc)
     })
   }
 }
@@ -16,5 +27,19 @@ var Message = {
 var MessageView = {
   toggleChatList: function(){
     $('.chat-box').toggleClass('expanded')
+  },
+  hideConversations: function(){
+    $(".conversation-options").addClass("inactive")
+  },
+  showConversations: function(){
+    $(".conversation-options").removeClass("inactive")
+  },
+  showConversation: function(){
+    $(".conversation.inactive").addClass("active")
+    $(".conversation.inactive").removeClass("inactive")
+  },
+  setConversationValues: function(name, imageSrc){
+    $(".conversation.active").find(".name")[0].innerHTML = name
+    $(".conversation.active").find("img").attr("src", imageSrc)
   }
 }
