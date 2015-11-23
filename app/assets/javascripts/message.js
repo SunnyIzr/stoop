e49ad6595c9cd97dbda43882cc27c687c91d1bf4
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-11-22 22:15:30 -0800

    functional adding of messaging: front-end ui crap

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,26 +3,43 @@ var MessageEvents = {
     this.chatListClick();
     this.conversationClick();
     this.submitMessageButtonClicked();
+    this.backButtonClick();
   },
   chatListClick: function(){
     $('.chat-bar .top-bar').click(function(e){
       e.preventDefault();
-      MessageView.toggleChatList();
+      if (!$(".chat-box").hasClass("expanded") || $(".conversation.active").length > 0){
+        MessageView.toggleChatList();
+      }
     })
   },
   conversationClick: function(){
     $('.conversation').click(function(e){
       var elementTargeted = $(e.currentTarget)
+      var userId = elementTargeted.closest(".conversation")[0].id
+      console.log(userId)
       var name = elementTargeted.find(".name")[0].innerHTML
       var imageSrc = elementTargeted.find("img")[0].getAttribute("src");
       MessageView.hideConversations()
       MessageView.showConversation()
-      MessageView.setConversationValues(name, imageSrc)
+      MessageView.setConversationValues(name, imageSrc, userId)
+    })
+  },
+  backButtonClick: function(){
+    $(".back-button").click(function(e){
+      console.log("clicked back button")
+      MessageView.hideConversation();
+      MessageView.showConversations();
     })
   },
   submitMessageButtonClicked: function(){
     $(".submit-message-button").click(function(e){
-
+      var userId = $(".conversation.active")[0].id
+      var body = $("textarea").last().val()
+      $.post( "messages", {recipients: userId, message: {body: body}}, function( data ) {
+        console.log("worked")
+        console.log(data)
+      });
     })
   }
 }
@@ -33,18 +50,37 @@ var Message = {
 var MessageView = {
   toggleChatList: function(){
     $('.chat-box').toggleClass('expanded')
+    if (!$(".chat-box").hasClass("expanded")){
+      this.hideConversation();
+      this.showConversations();
+    }
+  },
+  hideBackButton: function(){
+    $(".back-button").addClass("inactive")
+    $(".back-button").removeClass("active")
+  },
+  showBackButton: function(){
+    $(".back-button").addClass("active")
+    $(".back-button").removeClass("inactive")
   },
   hideConversations: function(){
-    $(".conversation-options").addClass("inactive")
+    $(".conversation-options").addClass("inactive");
+    this.showBackButton();
   },
   showConversations: function(){
     $(".conversation-options").removeClass("inactive")
+    this.hideBackButton();
   },
   showConversation: function(){
     $(".conversation.inactive").addClass("active")
     $(".conversation.inactive").removeClass("inactive")
   },
-  setConversationValues: function(name, imageSrc){
+  hideConversation: function(){
+    $(".conversation.active").addClass("inactive")
+    $(".conversation.active").removeClass("active")
+  },
+  setConversationValues: function(name, imageSrc, userId){
+    $(".conversation.active").attr("id", userId)
     $(".conversation.active").find(".name")[0].innerHTML = name
     $(".conversation.active").find("img").attr("src", imageSrc)
   }
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,9 +5,9 @@ class MessagesController < ApplicationController
   end
 
   def create
-    recipients = User.where(id: params['recipients'])
-    conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
-    flash[:success] = "Message has been sent!"
-    redirect_to conversation_path(conversation)
+    recipient = User.find(params['recipients'])
+    conversation = current_user.send_message(recipient, params[:message][:body], "Subject").conversation
+    render :json => conversation.messages
+    #redirect_to conversation_path(conversation)
   end
 end
