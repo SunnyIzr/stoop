874cd7495cd2c7409048bdf833a15c2333a6840a
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-11-23 21:46:12 -0800

    conversations being added dynamically when new messages sent

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,48 +2,80 @@ var MessageEvents = {
   init: function(){
     this.chatListClick();
     this.conversationClick();
-    this.submitMessageButtonClicked();
+    this.submitNewMessageButtonClicked();
+    this.newConversationButtonClick();
     this.backButtonClick();
   },
   chatListClick: function(){
     $('.chat-bar .top-bar').click(function(e){
       e.preventDefault();
+      if ($(".back-button").hasClass("inactive")){
       //if (!$(".chat-box").hasClass("expanded") || $(".conversation.active").length > 0){
         MessageView.toggleChatList();
-     // }
+      } else if ( !$(".conversation-list").hasClass("hidden") ){
+        MessageView.hideBackButton()
+      }
+    })
+  },
+  newConversationButtonClick: function(){
+    $(".new-conversation-button").click(function(e){
+      MessageView.hideConversations();
+      MessageView.hideNewConversationButton()
+      MessageView.showBuildingMemberList()
+      MessageView.showBackButton()
     })
   },
   conversationClick: function(){
     $('.conversation').click(function(e){
       var elementTargeted = $(e.currentTarget)
-      // nasty; won't scale well
-      var convoId = $(e.currentTarget).id
       var closestConversation = elementTargeted.closest(".conversation")[0]
-      $(closestConversation).height("432px")
-      var userId = closestConversation.id
-      var name = elementTargeted.find(".name")[0].innerHTML
-      var imageSrc = elementTargeted.find("img")[0].getAttribute("src");
-      MessageView.showConversationMessages()
-      var closestMessages = elementTargeted.find(".conversation-messages.active")[0]
-      $(closestMessages).height("383px")
+      if (elementTargeted.hasClass("building-member")){
+        var name = elementTargeted.find(".name")[0].innerHTML
+        var imageSrc = elementTargeted.find("img")[0].getAttribute("src");
+        var userId = closestConversation.id
+        MessageView.setNewConversationValues(name, imageSrc, userId)
+        MessageView.showNewConversation()
+        MessageView.hideBuildingMemberList()
+      } else {
+        var convoId = closestConversation.id
+        //$.get("/conversations/" + convoId, function( data ) {
+        //  $.each(data.messages, function(k, v) {
+        //    $('.conversation-messages.messages').append('<div>'+ v.body +'</div>');
+        //  });
+        //});
+      }
+//      // nasty; won't scale well
+//      MessageView.showConversationMessages()
+//      var closestMessages = elementTargeted.find(".conversation-messages.active")[0]
+//      $(closestMessages).height("383px")
       //MessageView.hideConversations()
       //MessageView.showConversation()
-      //MessageView.setConversationValues(name, imageSrc, userId, convoId)
     })
   },
   backButtonClick: function(){
     $(".back-button").click(function(e){
-      MessageView.hideConversation();
-      MessageView.showConversations();
+      if ( !$(".building-member-list").hasClass("hidden") ){
+        MessageView.hideBuildingMemberList();
+        MessageView.showConversations();
+        MessageView.showNewConversationButton();
+      } else if (!$(".conversation-new").hasClass("hidden")){
+        MessageView.hideNewConversation();
+        MessageView.showBuildingMemberList();
+      } else {
+        MessageView.hideConversation();
+        MessageView.showConversations();
+      }
     })
   },
-  submitMessageButtonClicked: function(){
+  submitNewMessageButtonClicked: function(){
     $(".submit-message-button").click(function(e){
-      var userId = $(".conversation.active")[0].id
-      var body = $("textarea").last().val()
+      var userId = $(".conversation-new")[0].id
+      var body = $("#new-message-text").val()
+      var name = $(".conversation-new").find(".name")[0].innerHTML
+      var avatar  = $(".conversation-new").find("img").attr("src")
       $.post( "messages", {recipients: userId, message: {body: body}}, function( data ) {
-        console.log("worked")
-        console.log(data)
+          var message = data[0];
+          $(".conversation-list")[0].insertAdjacentHTML('beforeend', "<div class='conversation' id=" + message.conversation_id + "> <a href='#'><img class='img-responsive' src='" + avatar + "'><p class='name'>" + name + "</p></a></div>")
       });
     })
   }
@@ -69,12 +101,10 @@ var MessageView = {
     $(".back-button").removeClass("inactive")
   },
   hideConversations: function(){
-    $(".conversation-options").addClass("inactive");
-    this.showBackButton();
+    $(".conversation-list").addClass("hidden");
   },
   showConversations: function(){
-    $(".conversation-options").removeClass("inactive")
-    this.hideBackButton();
+    $(".conversation-list").removeClass("hidden")
   },
   showConversation: function(){
     $(".conversation.inactive").addClass("active")
@@ -92,9 +122,28 @@ var MessageView = {
     $(".conversation-messages.active").addClass("inactive")
     $(".conversation-messages.active").removeClass("active")
   },
-  setConversationValues: function(name, imageSrc, userId){
-    $(".conversation.active").attr("id", userId)
-    $(".conversation.active").find(".name")[0].innerHTML = name
-    $(".conversation.active").find("img").attr("src", imageSrc)
+
+  showNewConversation: function(){
+    $(".conversation-new").removeClass("hidden")
+  },
+  hideNewConversation: function(){
+    $(".conversation-new").addClass("hidden")
+  },
+  setNewConversationValues: function(name, imageSrc, id){
+    $(".conversation-new").attr("id", id)
+    $(".conversation-new").find(".name")[0].innerHTML = name
+    $(".conversation-new").find("img").attr("src", imageSrc)
+  },
+  showNewConversationButton: function(){
+    $(".new-conversation-button").removeClass("hidden")
+  },
+  hideNewConversationButton: function(){
+    $(".new-conversation-button").addClass("hidden")
+  },
+  hideBuildingMemberList: function(){
+    $(".building-member-list").addClass("hidden")
+  },
+  showBuildingMemberList: function(){
+    $(".building-member-list").removeClass("hidden")
   }
 }
