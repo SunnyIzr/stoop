7681e11c8f00d54c0a8912d369c3ea15fde947d1
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-04-16 14:45:34 -0400

    Override devise sessions route for flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { :omniauth_callbacks => "callbacks", registrations: "users/registrations" }
+  devise_for :users, controllers: { :omniauth_callbacks => "callbacks", registrations: "users/registrations", sessions: 'users/sessions' }
 
   authenticated :user do
     root to: 'users#feed', as: :authenticated_root
   end
-  
+
   get '/feed/:filter' => 'users#filtered_feed'
-  
+
   #Static Pages Routes
   root to: 'static_pages#index'
   get '/financial-district' => 'static_pages#financial_district'
-  
-  
+
+
   #User Routes
   post '/users/search' => 'users#search'
   resources :users, only: [:show, :update, :index] do
@@ -19,19 +19,19 @@ Rails.application.routes.draw do
   end
   get '/users/:id/verify' => 'users#verify'
   get '/get-conversation/:cpty_id' => 'users#get_convo'
-  
+
   #Business Routes
   post '/yelp/search' => 'yelp#search'
   post '/businesses/search' => 'businesses#search'
   get '/unverified_businesses/:id' => 'businesses#show_unverified'
-  resources :businesses, only: [:index, :show, :update]  
-  
+  resources :businesses, only: [:index, :show, :update]
+
   #Events Routes
   put '/events/update_time/:id' => 'events#update_time'
   post '/invites/create-bulk' => 'invites#create_bulk'
   resources :events, only: [:index, :create, :show, :update]
   resources :invites, only: [:create,:update]
-  
+
   #Mailboxer Routes
   resources :messages, only: [:new, :create]
   resources :conversations, only: [:index, :show, :destroy] do
@@ -44,31 +44,31 @@ Rails.application.routes.draw do
       delete :empty_trash
     end
   end
-  
+
   #Post Routes
   resources :posts, only: [:create,:show]
-  
+
   #Comment Routes
   resources :comments, only: [:create]
-  
+
   #Like Routes
   resources :likes, only: [:create]
-  
+
   #Follow Routes
   resources :follows, only: [:create]
-  
+
   #Incentive and DiscountCode Routes
   resources :incentives, only: [:index,:show,:create,:update]
   resources :discount_codes, only: [:index,:show,:create,:update]
-  
+
   #Notification Routes
   resources :notification, only: [:create]
   post '/notifications/read-all' => 'notifications#read_all'
   post '/notifications/read-chat' => 'notifications#read_chats'
   post '/notifications/read' => 'notifications#read'
   get '/unread-chat' => 'notifications#unread_chat'
-  
-  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
